93a17797ff3f790183afd6ec46860282303141a4
SeieunYoo <isho48239@gmail.com>
2025-06-21 19:51:52 +0900

    feat: 핸들러 추가

--- apps/admin/public/mocks/http.ts
+++ apps/admin/public/mocks/http.ts
@@ -9,13 +9,16 @@ const port = 9090;
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // 클라이언트 주소
+    origin: [
+      "http://localhost:3000",
+      "https://local-mentor.study.gdschongik.com",
+    ],
     optionsSuccessStatus: 200,
     credentials: true,
   })
 );
 
 app.use(express.json());
-app.use(createMiddleware(...handlers));
+app.use(createMiddleware(...handlers) as any);
 
 app.listen(port, () => console.log(`Mock server is running on port: ${port}`));
