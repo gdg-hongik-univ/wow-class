9cce7f2d4c5694e7d5e9a0ead5f7953b824d5347
SeieunYoo <isho48239@gmail.com>
2025-07-23 22:18:56 +0900

    chore: og 관련 메타데이터 추가

--- apps/admin/constants/metaData.ts
+++ apps/admin/constants/metaData.ts
@@ -7,6 +7,8 @@ export const metaData: Metadata = {
     description:
       "와우클래스는 GDGoC Hongik이 제공하는 스터디 관리 플랫폼입니다.",
     images: ["/images/og-image.png"],
+    locale: "ko_KR",
+
     siteName: "와우클래스: 멘토 | GDGoC Hongik 스터디 서비스",
     type: "website",
   },
