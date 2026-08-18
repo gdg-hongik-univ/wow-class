9634a6015f94fbc630362e4cda0ab3590f633c86
소울치킨 <soulfever01@naver.com>
2026-08-18 11:44:23 +0900

    fix: baseurl alias에 맞춰 Modal.stories 수정

--- packages/ui/src/components/Modal/Modal.stories.tsx
+++ packages/ui/src/components/Modal/Modal.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { useOpenState } from "src/hooks";
 
+import { useOpenState } from "../../hooks";
 import Text from "../Text";
 import type { ModalProps } from ".";
 import Modal from ".";
